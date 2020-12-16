404d94bf84b45880d0a24878c25f311cb5222229
OVerGon <oscar.verdura@gmail.com>
2020-12-16 16:59:17 +0100

    Resolve 2020.15.2. Refactor all code

--- 2020/15/README.md
+++ 2020/15/README.md
@@ -2,12 +2,12 @@ use python3
 # problem 15
 https://adventofcode.com/2020/day/15
 1: Given your starting numbers, what will be the 2020th number spoken?
-2:
+2: Given your starting numbers, what will be the 30000000th number spoken?
 
 # results
 ```
 1: 410
-2:
+2: 238
 ```
 
 # input
--- 2020/15/resolve.py
+++ 2020/15/resolve.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 DATA=[7,12,1,0,16,2]
-END_TURN=2020
+END_TURN_1=2020
+END_TURN_2=30000000
 
 def get_age(arr, last_number):
     result=[]
@@ -11,21 +12,37 @@ def get_age(arr, last_number):
             break
     return result[0]-result[1]
 
-def problem_one():
-    spoken_numbers=DATA
-    turn=len(spoken_numbers)+1
-    while turn <= END_TURN:
-        last_number=spoken_numbers[-1]
-        if spoken_numbers.count(last_number) == 1:
-            spoken_numbers.append(0)
-        else:
-            spoken_numbers.append(get_age(spoken_numbers, last_number))
+def problem(data, end_turn):
+    init_numbers=data[:]
+    # number:[ix_1, ix_2, ix_3]
+    spoken_numbers={}
+    turn=1
+    last_number = None
+    for n in init_numbers:
+        spoken_numbers[n]=[turn]
+        last_number=n
         turn+=1
-
-    print("p1: "+ str(spoken_numbers[-1]))
-
-
-
+    while turn <= end_turn:
+        if last_number in spoken_numbers and len(spoken_numbers[last_number])==1:
+            arr=spoken_numbers[0]
+            arr.append(turn)
+            if len(arr)>2:
+                spoken_numbers[0]=arr[1:]
+            else:
+                spoken_numbers[0]=arr
+            last_number=0
+        else:
+            arr=spoken_numbers[last_number]
+            last_number=arr[-1]-arr[-2]
+            arr_ln=[turn]
+            if last_number in spoken_numbers:
+                arr_ln = spoken_numbers[last_number]
+                arr_ln.append(turn)
+            spoken_numbers[last_number]=arr_ln
+        turn+=1
+    result = "p1: " if end_turn == END_TURN_1 else "p2: "
+    print(result+ str(last_number))
 
 if __name__ == "__main__":
-    problem_one()
+    problem(DATA, END_TURN_1)
+    problem(DATA, END_TURN_2)
