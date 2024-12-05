26d7f0d8b166bfc6e4aa92ee39d2f6783881cc40
Óscar Verdura <oscar.verdura@clarity.ai>
2024-12-05 17:29:14 +0100

    Refactor

--- 2024/aoc2024/src/main/java/ovg/aoc2024/Day05.java
+++ 2024/aoc2024/src/main/java/ovg/aoc2024/Day05.java
@@ -2,7 +2,6 @@ package main.java.ovg.aoc2024;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import main.java.ovg.aoc2024.common.Utils;
